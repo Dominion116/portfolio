38fc0c592313489997d1d48e3dda9a51af6a0424
Oyewale Dominion <limbotech116@gmail.com>
2026-05-15 23:05:55 +0100

    feat: update resume redirection logic to use window.location.replace for better handling

--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 
 export default function ResumePage() {
   useEffect(() => {
-    window.location.href = process.env.NEXT_PUBLIC_RESUME_LINK || "/";
+    const resumeUrl =
+      process.env.NEXT_PUBLIC_RESUME_LINK || "/resume/dominion_resume.pdf";
+    window.location.replace(resumeUrl);
   }, []);
   return <div>Redirecting to the resume...</div>;
 }
